2d6cbfc9c2075881982220194fd76c8b945415e6
ayoub <ayoubhaddou1@gmail.com>
2022-06-07 14:31:31 +0200

    routes

--- App/A_iFruits/routes.py
+++ App/A_iFruits/routes.py
@@ -1,6 +1,19 @@
 from A_iFruits import app
 from flask import render_template
 
+# Page d'accueil (Description du produit)
 @app.route('/')
 def homepage():
     return render_template("home.html")
+
+# Page upload d'une image pour detection
+@app.route('/upload')
+def predict_upload():
+    return render_template("home.html")
+
+# Page detection d'objet en temps réel (webcam)
+@app.route('/live')
+def predict_live():
+    return render_template("home.html")
+
+
