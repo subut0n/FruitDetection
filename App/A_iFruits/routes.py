29f8a92ad8299aacb55e7a7d23159a8b465964ce
ayoub <ayoubhaddou1@gmail.com>
2022-06-07 17:58:12 +0200

    2 images predicted + input not working added

--- App/A_iFruits/routes.py
+++ App/A_iFruits/routes.py
@@ -14,6 +14,20 @@ def predict_upload():
 # Page detection d'objet en temps réel (webcam)
 @app.route('/live')
 def predict_live():
+    from imageai.Detection import ObjectDetection
+    import os
+
+    execution_path = os.getcwd()
+
+    detector = ObjectDetection()
+    detector.setModelTypeAsRetinaNet()
+    detector.setModelPath( os.path.join(execution_path , "A_iFruits/static/models_files/resnet50_coco_best_v2.1.0.h5"))
+    detector.loadModel()
+    detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "A_iFruits/static/images/src/image2.jpg"), output_image_path=os.path.join(execution_path , "A_iFruits/static/images/dest/imagenew2.jpg"))
+
+    for eachObject in detections:
+        print(eachObject["name"] , " : " , eachObject["percentage_probability"] )
+
     return render_template("home.html")
 
 
