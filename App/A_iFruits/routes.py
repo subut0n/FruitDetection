c211658b0fb9fb16648d1fd17b00e4cd9d2ef734
RudyBourez <bourez.rudy@gmail.com>
2022-06-09 15:10:46 +0200

    modèle sur flask

--- App/A_iFruits/routes.py
+++ App/A_iFruits/routes.py
@@ -1,11 +1,26 @@
 from A_iFruits import app
 from flask import render_template, Response , request , redirect, url_for , flash
-from imageai.Detection import ObjectDetection
-from imageai.Detection import VideoObjectDetection
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import os
-import cv2
 from .forms import uploadFile
 from werkzeug.utils import secure_filename
+
+
+list_labels = [
+    "Apple", "Apricot","Avocado","Banana", "Beetroot","Blueberry","Cabbage",
+    "Cactus","Cantaloupe","Caspicum","Carambula","Carrot","Cauliflower","Cherry",
+    "Chestnut","Clementine","Cocos","Corn","Cucumber","Dates","Eggplant","Fig",
+    "Garlic","Ginger","Granadilla","Grape","Grapefruit","Guava","Hazelnut",
+    "Huckleberry","Jalapeno","Kaki","Kiwi","Kohlrabi","Kumquats","Lemon","Limes",
+    "Lychee","Mandarine","Mango","Mangostan","Maracuja","Melon","Mulberry",
+    "Nectarine","Nut","Onion","Orange","Papaya","Passion","Peach","Pear",
+    "Peas","Pepino","Pepper","Physalis","Pineapple","Pitahaya","Plum","Pomegranate",
+    "Pomelo","Potato","Quince","Quince","Raddish","Rambutan","Raspberry","Redcurrant",
+    "Salak","Soybeans","Spinach","Strawberry","Tamarillo","Tangelo","Tomato","Turnip",
+    "Walnut","Watermelon"
+]
 
 # Page d'accueil (Description du produit)
 @app.route('/')
@@ -22,36 +37,37 @@ def upload_photo():
 
     form = uploadFile()
     execution_path = os.getcwd()
-    description = []
+    description=[]
     if form.validate_on_submit():
 
         if form.file.data :
-
             photo = form.file.data
             # f = secure_filename(photo.filename) # Dont need it
-            photo.save(os.path.join(
-                execution_path , "A_iFruits/static/images/src",  'file_upload.jpg'
-            ))
+            photo.save("App/A_iFruits/static/images/src/upload/file_upload.jpg")
+
+            model = tf.keras.models.load_model("Data/aifruits_model.h5")
+            predicting_img = ImageDataGenerator(
+                preprocessing_function=tf.keras.applications.resnet50.preprocess_input,
+                dtype=tf.float32
+            )
+            img_transform = predicting_img.flow_from_directory(
+                directory="App/A_iFruits/static/images/src",
+                target_size=(75, 75)
+            )
+            description.append(list_labels[np.argmax(model.predict(img_transform))])
+            # print(prediction)
+            # detections = detector.detectObjectsFromImage(input_image=os.path.join(
+            #     execution_path , "A_iFruits/static/images/src",  'file_upload.jpg'
+            # ), output_image_path=os.path.join(execution_path , "A_iFruits/static/images/dest/predict_upload_file.jpg"))
+
 
             
-            
-            detector = ObjectDetection()
-            detector.setModelTypeAsRetinaNet()
-            detector.setModelPath( os.path.join(execution_path , "A_iFruits/static/models_files/resnet50_coco_best_v2.1.0.h5"))
-            detector.loadModel()
-
-            detections = detector.detectObjectsFromImage(input_image=os.path.join(
-                execution_path , "A_iFruits/static/images/src",  'file_upload.jpg'
-            ), output_image_path=os.path.join(execution_path , "A_iFruits/static/images/dest/predict_upload_file.jpg"))
-
-
-            
-            for eachObject in detections:
-                text = eachObject["name"] , " : " , eachObject["percentage_probability"] 
-                description.append(text)
-            flash('File predicted', category='success')
-        else:
-            flash('Please load an image', category="error" )
+        #     for eachObject in detections:
+        #         text = eachObject["name"] , " : " , eachObject["percentage_probability"] 
+        #         description.append(text)
+        #     flash('File predicted', category='success')
+        # else:
+        #     flash('Please load an image', category="error" )
 
 
     return render_template("upload_photo.html", form=form, description=description)
