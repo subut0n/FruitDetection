379d7f0f2439de8371913eada05336896d518d32
subut0n <a.dzikowski@outlook.fr>
2022-06-09 15:14:01 +0200

    adrien setup

--- App/A_iFruits/routes.py
+++ App/A_iFruits/routes.py
@@ -6,6 +6,7 @@ import os
 import cv2
 from .forms import uploadFile
 from werkzeug.utils import secure_filename
+import matplotlib as plt
 
 # Page d'accueil (Description du produit)
 @app.route('/')
@@ -58,12 +59,56 @@ def upload_photo():
 
 # -------------  Page upload d'une video pour detection
 
+execution_path = os.getcwd()
+
+color_index = {'bus': 'red', 'handbag': 'steelblue', 'giraffe': 'orange', 'spoon': 'gray', 'cup': 'yellow', 'chair': 'green', 'elephant': 'pink', 'truck': 'indigo', 'motorcycle': 'azure', 'refrigerator': 'gold', 'keyboard': 'violet', 'cow': 'magenta', 'mouse': 'crimson', 'sports ball': 'raspberry', 'horse': 'maroon', 'cat': 'orchid', 'boat': 'slateblue', 'hot dog': 'navy', 'apple': 'cobalt', 'parking meter': 'aliceblue', 'sandwich': 'skyblue', 'skis': 'deepskyblue', 'microwave': 'peacock', 'knife': 'cadetblue', 'baseball bat': 'cyan', 'oven': 'lightcyan', 'carrot': 'coldgrey', 'scissors': 'seagreen', 'sheep': 'deepgreen', 'toothbrush': 'cobaltgreen', 'fire hydrant': 'limegreen', 'remote': 'forestgreen', 'bicycle': 'olivedrab', 'toilet': 'ivory', 'tv': 'khaki', 'skateboard': 'palegoldenrod', 'train': 'cornsilk', 'zebra': 'wheat', 'tie': 'burlywood', 'orange': 'melon', 'bird': 'bisque', 'dining table': 'chocolate', 'hair drier': 'sandybrown', 'cell phone': 'sienna', 'sink': 'coral', 'bench': 'salmon', 'bottle': 'brown', 'car': 'silver', 'bowl': 'maroon', 'tennis racket': 'palevilotered', 'airplane': 'lavenderblush', 'pizza': 'hotpink', 'umbrella': 'deeppink', 'bear': 'plum', 'fork': 'purple', 'laptop': 'indigo', 'vase': 'mediumpurple', 'baseball glove': 'slateblue', 'traffic light': 'mediumblue', 'bed': 'navy', 'broccoli': 'royalblue', 'backpack': 'slategray', 'snowboard': 'skyblue', 'kite': 'cadetblue', 'teddy bear': 'peacock', 'clock': 'lightcyan', 'wine glass': 'teal', 'frisbee': 'aquamarine', 'donut': 'mincream', 'suitcase': 'seagreen', 'dog': 'springgreen', 'banana': 'emeraldgreen', 'person': 'honeydew', 'surfboard': 'palegreen', 'cake': 'sapgreen', 'book': 'lawngreen', 'potted plant': 'greenyellow', 'toaster': 'ivory', 'stop sign': 'beige', 'couch': 'khaki'}
+
+
+resized = False
+
+def forFrame(frame_number, output_array, output_count, returned_frame):
+
+    plt.clf()
+
+    this_colors = []
+    labels = []
+    sizes = []
+
+    counter = 0
+
+    for eachItem in output_count:
+        counter += 1
+        labels.append(eachItem + " = " + str(output_count[eachItem]))
+        sizes.append(output_count[eachItem])
+        this_colors.append(color_index[eachItem])
+
+    global resized
+
+    if (resized == False):
+        manager = plt.get_current_fig_manager()
+        manager.resize(width=1000, height=500)
+        resized = True
+
+    plt.subplot(1, 2, 1)
+    plt.title("Frame : " + str(frame_number))
+    plt.axis("off")
+    plt.imshow(returned_frame, interpolation="none")
+
+    plt.subplot(1, 2, 2)
+    plt.title("Analysis: " + str(frame_number))
+    plt.pie(sizes, labels=labels, colors=this_colors, shadow=True, startangle=140, autopct="%1.1f%%")
+
+    plt.pause(0.01)
+
+
 @app.route('/upload-video', methods=['POST','GET'])
 def upload_video():
 
     form = uploadFile()
     execution_path = os.getcwd()
     description = []
+
+    
     if form.validate_on_submit():
 
         if form.file.data :
@@ -85,7 +130,7 @@ def upload_video():
 
             video_path = detector.detectObjectsFromVideo(camera_input=os.path.join(
                 execution_path , "A_iFruits/static/images/dest",  'file_upload.avi'), output_file_path=os.path.join(execution_path, "A_iFruits/static/images/dest/camera_detected_1")
-                                            , frames_per_second=29, log_progress=True, minimum_percentage_probability=40)
+                                            , frames_per_second=29, per_frame_function=forFrame, log_progress=True, minimum_percentage_probability=40)
 
             
             flash('File predicted', category='success')
