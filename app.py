ce303c4dfb9387adf00733906fec55be98c16c63
ayoub <ayoubhaddou1@gmail.com>
2022-07-05 22:48:18 +0200

    port in .env

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from App.A_iFruits import app
+import os 
+from dotenv import load_dotenv
+load_dotenv() 
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=os.getenv('PORT'))
