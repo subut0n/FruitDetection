e999079893fe0e82d84812b5b7823ac8de90820b
ayoub <ayoubhaddou1@gmail.com>
2022-07-05 19:54:32 +0200

    probleme de funfact régler

--- App/A_iFruits/prediction.py
+++ App/A_iFruits/prediction.py
@@ -5,6 +5,7 @@ import json
 from tflite_support import metadata
 import cv2
 import tensorflow as tf
+import pandas as pd 
 
 
 Interpreter = tf.lite.Interpreter
@@ -287,9 +288,9 @@ def visualize(
 
     # Draw label and score
     category = detection.categories[0]
-    class_name = category.label
+    class_num = category.label
     probability = round(category.score, 2)
-    result_text = class_name + ' (' + str(probability) + ')'
+    result_text = class_num + ' (' + str(probability) + ')'
     text_location = (_MARGIN + detection.bounding_box.left,
                      _MARGIN + _ROW_SIZE + detection.bounding_box.top)
     cv2.putText(image, result_text, text_location, cv2.FONT_HERSHEY_PLAIN,
@@ -298,17 +299,111 @@ def visualize(
   return image
 
 
-
-def return_class_names ( image: np.ndarray, detections: List[Detection]) :
-
+def return_class_nums ( image: np.ndarray, detections: List[Detection]) : # For tflite 
   result_text = []
   for detection in detections:
     # Draw label and score
     category = detection.categories[0]
     probability = round(category.score, 2)
-    class_name = category.label
-    text = class_name + ' (' + str(probability) + ')'
-
+    class_num = category.label
+    text = class_num + ' (' + str(probability) + ')'
     result_text.append(text)
-
   return result_text
+
+
+labels = {'Apple' : 0,
+ 'Apricot' : 1,
+ 'Avocado' : 2,
+ 'Banana' : 3,
+ 'Beetroot' : 4,
+ 'Blueberry' : 5,
+ 'Cactus' : 6,
+ 'Cantaloupe' : 7,
+ 'Carambula' : 8,
+ 'Cauliflower': 9,
+ 'Cherry' : 10,
+ 'Chestnut' : 11,
+ 'Clementine' : 12,
+ 'Cocos' : 13,
+ 'Corn' : 14,
+ 'Dates' : 15,
+ 'Eggplant' : 16,
+ 'Ginger' : 17,
+ 'Granadilla' : 18,
+ 'Grape' : 19,
+ 'Grapefruit' : 20,
+ 'Guava' : 21,
+ 'Hazelnut' : 22,
+ 'Huckleberry' : 23,
+ 'Kaki' : 24,
+ 'Kiwi' : 25,
+ 'Kohlrabi' : 26,
+ 'Kumquats' : 27,
+ 'Lemon' : 28,
+ 'Limes' : 29,
+ 'Lychee' : 30,
+ 'Mandarine' : 31,
+ 'Mango' : 32,
+ 'Mangostan' : 33,
+ 'Maracuja' : 34,
+ 'Melon' : 35,
+ 'Mulberry' : 36,
+ 'Nectarine' : 37,
+ 'Nut' : 38,
+ 'Onion' : 39,
+ 'Orange' : 40,
+ 'Papaya' : 41,
+ 'Passion' : 42,
+ 'Peach' : 43,
+ 'Pear' : 44,
+ 'Pepino' : 45,
+ 'Pepper' : 46,
+ 'Physalis' : 47,
+ 'Pineapple' : 48,
+ 'Pitahaya' : 49,
+ 'Plum' : 50,
+ 'Pomegranate' : 51 ,
+ 'Pomelo' : 52,
+ 'Potato' : 53,
+ 'Pumpkin' : 54,
+ 'Quince' : 55,
+ 'Rambutan' : 56,
+ 'Raspberry' : 57,
+ 'Redcurrant' : 58,
+ 'Salak' : 59,
+ 'Strawberry' : 60,
+ 'Tamarillo' : 61,
+ 'Tangelo' : 62,
+ 'Tomato' : 63,
+ 'Walnut' : 64}
+
+def return_class_yolo(): # Return list o string for predictions 
+    pred = []
+    import os 
+    print(os.getcwd())
+    with open("App/A_iFruits/static/yolov5/runs/detect/yolo_foodex/labels/file_upload.txt","r+") as f :
+        for i in f.readlines():
+            class_num = i.split()[0]
+            class_num = query_class_name(class_num)
+            operation = f'{class_num}: {i.split()[5]}'
+            pred.append(operation)
+        f.truncate(0) 
+    return pred
+
+def query_class_name(pred):
+    for key, value in labels.items():
+        if int(pred) == int(value):
+            return key 
+
+
+
+
+def return_fun_fact(pred_yolo): # Return list of string for funfact prediction
+  df = pd.read_csv('Data/fruits_description.csv')
+  labels = [i.split(':')[0] for i in pred_yolo]
+  unique = list(set(labels))
+  funfact_text = []
+  for name in unique : 
+      funfact_text.append(df[df.name == name].description.values[0])
+  return funfact_text
+
--- App/A_iFruits/routes.py
+++ App/A_iFruits/routes.py
@@ -4,50 +4,37 @@ import cv2
 import os
 from .forms import uploadFile
 import pandas as pd 
-
+from .prediction import return_class_yolo, return_fun_fact
 
 execution_path = os.getcwd()
 base_app = execution_path + '/App/A_iFruits/'
 base_model = base_app + 'static/yolov5/'
 
-list_funfact = pd.read_csv('Data/fruits_description.csv')
 # Page d'accueil (Description du produit)
 @app.route('/')
 def homepage():
     return render_template("home.html")
 
-
-
 # -------------  Page upload d'une image pour detection
-
 @app.route('/upload-photo', methods=['POST','GET'])
 def upload_photo():
-
 
     form = uploadFile()
     description=[]
     funfact_text=[]
-    sent_text = []
-    unique_class=[]
-    counter = []
-
 
     if form.validate_on_submit():
-
         if form.file.data :
-
             photo = form.file.data
             # f = secure_filename(photo.filename) # Dont need it
             photo.save("App/A_iFruits/static/images/src/upload/file_upload.jpg")
-            os.system(f'python {base_model}detect.py --source {base_app}static/images/src/upload/file_upload.jpg --weights {base_model}best5.pt --conf 0.5 --name yolo_foodex')
-            
-            # Run here the commande cli for yolo
-        #     flash('File predicted', category='success')
-        # else:
-        #     flash('Please load an image', category="error" )
-
-
-    return render_template("upload_photo.html", form=form, description=description, funfact_text=funfact_text, unique_class=unique_class, counter=counter)
+            os.system(f'python {base_model}detect.py --source {base_app}static/images/src/upload/file_upload.jpg --weights {base_model}best2.pt --conf 0.6 --name yolo_foodex --save-txt --save-conf')
+            description = return_class_yolo()
+            funfact_text = return_fun_fact(description)
+            flash('File predicted', category='success')
+        else:
+            flash('Please load an image', category="error" )
+    return render_template("upload_photo.html", form=form, description=description, funfact_text=funfact_text)
 
 # -------------  Page upload d'une video pour detection
 
@@ -58,65 +45,35 @@ def upload_video():
     execution_path = os.getcwd()
     description = []
     if form.validate_on_submit():
-
         if form.file.data :
             print(form.file.data)
             photo = form.file.data
-            photo.save(os.path.join(
-                execution_path , "App/A_iFruits/static/images/dest",  'file_upload.avi'
-            ))
-            os.system(f'python {base_model}detect.py --source {base_app}static/images/dest/file_upload.jpg --weights {base_model}best5.pt --conf 0.5 --name yolo_foodex')
+            photo.save(os.path.join(base_app, "static/images/dest/file_upload.avi"))
+            os.system(f'python {base_model}detect.py --source {base_app}static/images/dest/file_upload.jpg --weights {base_model}best2.pt --conf 0.5 --name yolo_foodex --save-txt --save-conf')
             flash('Video predicted', category='success')
-            redirect(url_for('home.html')) # Page pour visualiser la vidéo upload et predite à faire (voir page live.html)
+            redirect(url_for('home.html')) # Page pour visualiser la vidéo qui vient d'être predite (voir page live.html)
         else:
             flash('Please load an image', category="error" )
-
     return render_template("upload_video.html", form=form, description=description)
     
+# ------------- Page detection d'objet en temps réel (webcam)
+@app.route('/live')
+def predict_live():
+    return render_template("live.html")
 
 # Fonction generateur de video 
 def gen(video):
-
     while True:
         success, image = video.read()
-
         ret, jpeg = cv2.imencode('.jpg', image)
-
         frame = jpeg.tobytes()
-        
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
-
-@app.route('/video_feed2')
-def video_prediction():   
-
-    execution_path = os.getcwd()
-    video_path = os.path.join(execution_path, "App/A_iFruits/static/images/dest/file_upload.avi")
-    video = cv2.VideoCapture(video_path) 
-    
-    return Response(gen(video),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
-
-
-
-# ------------- Page detection d'objet en temps réel (webcam)
-
-
-
-@app.route('/live')
-def predict_live():
-
-    flash('Suceess', category='success')
-    return render_template("live.html")
-
-
-
 @app.route('/video_feed')
 def video_live():   
-    os.system(f'python {base_model}detect.py --source 0 --weights {base_model}best5.pt --conf 0.5')
-    
+    os.system(f'python {base_model}detect.py --source 0 --weights {base_model}best2.pt --conf 0.5')
     video = cv2.VideoCapture(f'{base_model}runs/detect/exp/0.mp4') 
-
+    # video = cv2.VideoCapture(0) 
     return Response(gen(video),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
